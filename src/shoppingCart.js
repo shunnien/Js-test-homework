d739ce3ddb6e962b27475a4f967d82805e7d4d2b
allen_yu <wiamyu@gmail.com>
2017-09-18 02:15:26 +0800

    綠燈 -  VIP 會員，購物 499 元，沒有優惠

---
+++ src/shoppingCart.js
@@ -0,0 +1,25 @@
+function ShoppingCart() {}
+
+
+
+ShoppingCart.prototype.getMemberType = function () {
+    return "";
+};
+
+ShoppingCart.prototype.getTotalPrice = function (products) {
+    let totalPrice = 0;
+    products.forEach(function (element) {
+        if (element.unitPrice && element.quantity) {
+            totalPrice += element.unitPrice * element.quantity;
+        }
+    }, this);
+    switch (this.getMemberType()) {
+        case "VIP":
+            return totalPrice >= 500 ? totalPrice * 0.8 : totalPrice;
+        default:
+            return totalPrice >= 1000 && products.length >= 3 ? totalPrice * 0.85 : totalPrice;
+    }
+
+};
+
+module.exports = ShoppingCart;
