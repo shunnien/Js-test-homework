4150aadebf2ddcf2c96ac6224565e228e0da6b57
allen_yu <wiamyu@gmail.com>
2017-09-18 02:21:51 +0800

    綠燈 -  VIP 會員，購物滿 500 元，有 8 折優惠，總金額應為 400 元

--- tests/shoppingCart.spec.js
+++ tests/shoppingCart.spec.js
@@ -26,6 +26,24 @@ describe('shoppingCart', () => {
             // Assert
             actual.should.equal(expected);
         });
+        it(' VIP 會員，購物滿 500 元，有 8 折優惠，總金額應為 400 元', () => {
+            // Arrange
+            const dataset = [{
+                productName: 'AAA',
+                unitPrice: 500,
+                quantity: 1
+            }];
+            const expected = 400;
+            let actual = 0;
+            let shoppinCart = new ShoppingCart();
 
+            shoppinCart.getMemberType = sinon.stub().returns('VIP');
+
+            // Act
+            actual = shoppinCart.getTotalPrice(dataset);
+
+            // Assert
+            actual.should.equal(expected);
+        });
     });
 });
