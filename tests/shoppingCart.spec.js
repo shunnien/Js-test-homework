84bcf5d5622f2e2501250aad5794681283170821
allen_yu <wiamyu@gmail.com>
2017-09-18 09:49:16 +0800

    紅燈 - 一般會員，購物 3 項共 1000 元，折扣後應為 1000 元

--- tests/shoppingCart.spec.js
+++ tests/shoppingCart.spec.js
@@ -34,6 +34,59 @@ describe('shoppingCart', () => {
             expected: 400
         }
     ];
+    const normalDataset = [{
+            products: [{
+                unitPrice: 999,
+                quantity: 1
+            }],
+            expected: 999
+        },
+        {
+            products: [{
+                unitPrice: 300,
+                quantity: 1
+            }, {
+                unitPrice: 300,
+                quantity: 1
+            }, {
+                unitPrice: 300,
+                quantity: 1
+            }, {
+                unitPrice: 99,
+                quantity: 1
+            }],
+            expected: 999
+        },
+        {
+            products: [{
+                unitPrice: 300,
+                quantity: 1
+            }, {
+                unitPrice: 300,
+                quantity: 1
+            }, {
+                unitPrice: 300,
+                quantity: 1
+            }, {
+                unitPrice: 100,
+                quantity: 1
+            }],
+            expected: 850
+        }, ,
+        {
+            products: [{
+                unitPrice: 500,
+                quantity: 1
+            }, {
+                unitPrice: 400,
+                quantity: 1
+            }, {
+                unitPrice: 100,
+                quantity: 1
+            }],
+            expected: 1000
+        }
+    ];
 
     vipDataset.forEach(function (element) {
 
@@ -50,6 +103,31 @@ describe('shoppingCart', () => {
             // Arrange
             let shoppingCart = new ShoppingCart();
             shoppingCart.getMemberType = sinon.stub().returns('VIP');
+
+            // Act
+            let actual = shoppingCart.getTotalPrice(element.products);
+
+            // Assert
+            actual.should.equal(element.expected);
+
+        });
+    });
+
+    normalDataset.forEach(function (element) {
+
+        const productsTotalPrice = () => {
+            let result = 0;
+            if (element && Array.isArray(element.products)) {
+                element.products.forEach(function (item) {
+                    result += item.unitPrice * item.quantity;
+                });
+            }
+            return result;
+        };
+        it(`一般會員，購物 ${element.products.length} 項共 ${productsTotalPrice()} 元，折扣後應為 ${element.expected} 元`, () => {
+            // Arrange
+            let shoppingCart = new ShoppingCart();
+            shoppingCart.getMemberType = sinon.stub().returns('Normal');
 
             // Act
             let actual = shoppingCart.getTotalPrice(element.products);
